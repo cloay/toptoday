7c0c5d609a4b8bba4b36858c8079265d18bb7fd1
cloay <shangrody@gmail.com>
2012-10-25 21:31:59 +0800

    Initial Commit

---
+++ ReferenceNews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReferenceNews
+//
+//  Created by cloay on 12-10-25.
+//  Copyright (c) 2012年 Cloay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReferenceNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReferenceNews
+//
+//  Created by cloay on 12-10-25.
+//  Copyright (c) 2012年 Cloay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
